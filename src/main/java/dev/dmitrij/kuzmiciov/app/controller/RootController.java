02b76329e61c9451cfbd7dfe9a5d068afa5b4085
Dmitrij Kuzmiciov <dmitrij.kuzmiciov@gmail.com>
2020-12-27 16:20:23 +0200

    Add edit group name button

--- src/main/java/dev/dmitrij/kuzmiciov/app/controller/RootController.java
+++ src/main/java/dev/dmitrij/kuzmiciov/app/controller/RootController.java
@@ -3,18 +3,19 @@ package dev.dmitrij.kuzmiciov.app.controller;
 import dev.dmitrij.kuzmiciov.app.App;
 import dev.dmitrij.kuzmiciov.app.RootTable;
 import dev.dmitrij.kuzmiciov.app.data.Group;
+import dev.dmitrij.kuzmiciov.app.util.Regexes;
 import dev.dmitrij.kuzmiciov.app.util.file.Loader;
 import dev.dmitrij.kuzmiciov.app.util.file.Saver;
 import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.*;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Priority;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 
 import java.util.ArrayList;
 
@@ -43,19 +44,63 @@ public final class RootController extends Controller {
         closeButton.setOnAction(e -> App.close());
 
         groupName = new TextField();
-        groupName.setStyle("-fx-alignment: center; -fx-background-color: transparent; -fx-background-insets: 0px");
+        var groupNameDefaultStyle = groupName.getStyle();
+        var groupNameStyle = "-fx-background-color: transparent; -fx-background-insets: 0px";
+        groupName.setStyle(groupNameStyle);
         groupName.setEditable(false);
 
+        // Making it commit change on ENTER
+        groupName.setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ENTER)
+                groupName.getParent().requestFocus();
+        });
+
+        // Handling group name editing
+        groupName.focusedProperty().addListener((obsVal, wasFocused, nowFocused) -> {
+            if(!nowFocused) {
+                if(groupName.getText().matches(Regexes.GROUP_EN.regex)) {
+                    var selModel = groupChoiceBox.getSelectionModel();
+                    int prevIndex = selModel.getSelectedIndex();
+                    selModel.clearSelection();
+                    selModel.clearAndSelect(prevIndex);
+
+                    groupName.setStyle(groupNameStyle);
+                    groupName.setEditable(false);
+                } else {
+                    groupName.setText("");
+                    groupName.requestFocus();
+                }
+            }
+        });
+
+        // Creating the 'edit group name' button
+        var editImage = new Image("/edit_icon.png", 16, 16, true, true);
+        var editButton = new Button();
+        editButton.setGraphic(new ImageView(editImage));
+        editButton.setVisible(false);
+        editButton.setTooltip(new Tooltip("Edit group name"));
+        editButton.setOnAction(e -> {
+            groupName.setStyle(groupNameDefaultStyle);
+            groupName.setEditable(true);
+            groupName.requestFocus();
+        });
+
+        // Wrapping group name and the button in a HBox
+        var groupNameWrapper = new HBox(10, editButton, groupName);
+        groupNameWrapper.setAlignment(Pos.CENTER);
+
+        // Creating the table
         table = new RootTable();
         table.setEditable(true);
 
-        var vbox = new VBox(10, groupName, table);
-        root.setCenter(vbox);
+        // Adding the table are to the parent node
+        var tableWrapper = new VBox(10, groupNameWrapper, table);
+        root.setCenter(tableWrapper);
+        BorderPane.setMargin(tableWrapper, new Insets(20));
 
         // Making sure that only the table expands vertically
         VBox.setVgrow(groupName, Priority.NEVER);
         VBox.setVgrow(table, Priority.ALWAYS);
-
 
         // Binding groupList to observable list
         groupChoiceBox.setItems(FXCollections.observableArrayList(groupList));
@@ -71,25 +116,12 @@ public final class RootController extends Controller {
 
                 if(newValue == null) {
                     groupName.clear();
-                    groupName.setEditable(false);
+                    editButton.setVisible(false);
                 } else {
                     groupName.textProperty().bindBidirectional(newValue.nameProperty());
                     table.setItems(FXCollections.observableList(newValue.getStudents()));
-                    if (!groupName.isEditable())
-                        groupName.setEditable(true);
+                    editButton.setVisible(true);
                 }
-            }
-        });
-        groupName.setOnKeyPressed(keyEvent -> {
-            if(keyEvent.getCode() == KeyCode.ENTER)
-                Platform.runLater(() -> groupName.getParent().requestFocus());
-        });
-        groupName.focusedProperty().addListener((obsVal, wasFocused, nowFocused) -> {
-            if(!nowFocused) {
-                var selModel = groupChoiceBox.getSelectionModel();
-                int prevIndex = selModel.getSelectedIndex();
-                selModel.clearSelection();
-                selModel.clearAndSelect(prevIndex);
             }
         });
     }
