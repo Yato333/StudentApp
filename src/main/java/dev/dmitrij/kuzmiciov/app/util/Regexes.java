dcd657b142398a9d23e08eab9d9403caa2f6ac2e
Dmitrij Kuzmiciov <dmitrij.kuzmiciov@gmail.com>
2020-12-27 16:52:44 +0200

    Change Group name regex to allow spaces

--- src/main/java/dev/dmitrij/kuzmiciov/app/util/Regexes.java
+++ src/main/java/dev/dmitrij/kuzmiciov/app/util/Regexes.java
@@ -3,9 +3,9 @@ package dev.dmitrij.kuzmiciov.app.util;
 import org.intellij.lang.annotations.Language;
 
 public enum Regexes {
-    GROUP_EN("^[A-Za-z0-9]+$"),
+    GROUP_EN("^[A-Za-z0-9 ]+$"),
     NAME_EN("^[A-Z][a-z]+ [A-Z][a-z]+$"),
-    GROUP_LT("^[A-ZĄČĖĮŠŪŽa-ząčęėįšųūž0-9]+$"),
+    GROUP_LT("^[A-ZĄČĖĮŠŪŽa-ząčęėįšųūž0-9 ]+$"),
     NAME_LT("^[A-ZĄČĖĮŠŲŪŽ][a-ząčęėįšųūž]+ [A-ZĄČĖĮŠŲŪŽ][a-ząčęėįšųūž]+$");
 
     @Language("regexp")
